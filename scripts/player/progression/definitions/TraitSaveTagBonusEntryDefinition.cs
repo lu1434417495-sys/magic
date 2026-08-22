ab2afeed71d1d078a4f436fb809a4df1b621c05c
lu <lu1434417495@gmail.com>
2026-08-22 23:13:11 +0800

    fix: add trait save tag projection types

---
+++ scripts/player/progression/definitions/TraitSaveTagBonusEntryDefinition.cs
@@ -0,0 +1,42 @@
+using System;
+using Godot;
+
+public sealed class TraitSaveTagBonusEntryDefinition
+{
+    public TraitSaveTagBonusEntryDefinition(
+        StringName saveTag,
+        int bonus,
+        StringName stackMode
+    )
+    {
+        SaveTag = saveTag;
+        Bonus = bonus;
+        StackMode = stackMode;
+    }
+
+    public StringName SaveTag { get; }
+    public int Bonus { get; }
+    public StringName StackMode { get; }
+
+    internal TraitSaveTagBonusStackModeKind StackModeKind =>
+        TraitContentRules.ToSaveTagBonusStackModeKind(StackMode);
+
+    internal static TraitSaveTagBonusEntryDefinition FromResource(
+        TraitSaveTagBonusEntryDef source,
+        string path
+    )
+    {
+        ArgumentNullException.ThrowIfNull(source);
+        ProgressionDefinitionProjection.RequireKnown(
+            TraitContentRules.ToSaveTagBonusStackModeKind(source.stack_mode)
+                != TraitSaveTagBonusStackModeKind.Unknown,
+            $"{path}.stack_mode",
+            source.stack_mode
+        );
+        return new TraitSaveTagBonusEntryDefinition(
+            source.save_tag,
+            source.bonus,
+            source.stack_mode
+        );
+    }
+}
